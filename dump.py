2691abcc0a42c82a753f84e1cd5b0f82f3d2ff4e
gamefreakoneone <amogh@outlook.com>
2020-07-25 22:26:27 +0530

    Upload

---
+++ dump.py
@@ -0,0 +1,8 @@
+# the following line reads the string to work with from the input, do not modify it, please
+input_str = input()
+lst=str()
+for i in len(input_str):
+    lst.append(input_str[i])
+
+print(lst)
+# your code here
